4e7e1ae5ca64b359e60c6b7ecea6a9f5911982d5
Elena Erofeeva <eerofeeva8@gmail.com>
2021-01-24 10:39:19 -0600

    WIP: code for barchart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,60 +1,96 @@
 var samplesNames;
 var metadata;
+var resultsarray;
 var teststring;
+var values;
+var ids;
+var hovertext;
 
-function getSamples(){
-    // d3.json("samples.json").then(function(data) {
-        
-    //     samplesNames = data.samples;
-    //     console.log("in getsamples, ");
-    //     console.log(samplesNames);
-    //     init();
-    // });
-};
-
-function getMetadata(){
-};
-
-function optionChanged(selected_sample)
+function getResults(sampleId)
 {
-    var resultsarray= metadata.filter(sampleobject => sampleobject.id == sample);
+    resultsarray= metadata.filter(sampleobject => sampleobject.id == sampleId)[0];
+
+    // Use `sample_values` as the values for the bar chart. - my Y values
+    values = resultsarray.sample_values;
+    // Use `otu_ids` as the labels for the bar chart. - my X values
+    ids = resultsarray.otu_ids;     
+    // Use `otu_labels` as the hovertext for the chart.
+    hovertext = resultsarray.otu_labels;
+
     console.log(resultsarray);
+}
+
+function optionChanged(sampleId)
+{
+    console.log("selected_sample " + sampleId);
+    getResults(sampleId);
+    console.log(resultsarray);
+    setMetadata();
+}
+
+function setMetadata()
+{
     var sample_metadata = d3.select("#sample-metadata");
     sample_metadata.html("");
-    Object.entries(resultsarray[0]).forEach(([key, value]) => {
+    Object.entries(resultsarray).forEach(([key, value]) => {
         sample_metadata.append("h6").text(`${key}: ${value}`);
     });
+}
+
+function setBarChart()
+{
+    var trace_bar = {
+        x: ids,
+        y: values,
+        text: hoverText,
+        type: 'bar',
+        orientation: 'h'
+    };
+ 
+ Plotly.newPlot('bar', [trace_bar])
+}
+
+function setBubbleChart(sample)
+{
+    var trace_bubble = {
+        x: ids,
+        y: values,
+        text: hoverText,
+        mode: 'markers',
+        marker: {
+            size: values,
+            color: ids
+        }
+    };
+    Plotly.newPlot('bubble', [trace_bubble]);
 }
 
 function init()
 {
    //initialize selector
    var selDataset = d3.select("#selDataset");
-
-   console.log("in init "); 
-   console.log(samplesNames);
-
-   samplesNames.forEach((sample) => {
+    console.log(samplesNames[0]);
+    samplesNames.forEach((sample) => {
     selDataset
       .append("option")
       .text(sample)
       .property("value", sample);
     });
-    
-    //initialize metadata
-    getMetadata();
-    
+
+    //initialize metadata through optionChanged
+    getResults(samplesNames[0]);
+    setMetadata();
     //initialize bar chart
-    //initBarChart();    
+    setBarChart();    
 
     //initialize bubble chart
-   // initBubbleChart();
+    //setBubbleChart(samplesNames[0]);
 }
 
-//initialize the site with all elements
+
+//assign samples and metadata globals
 d3.json("samples.json").then(function(data) {
-        
-    samplesNames = data.samples;
+    samplesNames = data.names;
     metadata = data.metadata;
     init();
 });
